b361e36aacfb957a54a60fefdcfce13eac18c89b
acwajega@wacsoftsolutions.com <acwajega@gmail.com>
2018-07-22 13:39:25 +0300

    f

--- controllers/accounts_controller.js
+++ controllers/accounts_controller.js
@@ -34,11 +34,11 @@ qry_action.query('select * from admin_users where au_username = ? and au_passwor
 
 	if (err){
 			 res.end(JSON.stringify({ resp:"err",err: 'An error occured while executing the querry' }));
-			 console.log('executing query 1');
+			 
 		}
 		else
 		{
-			console.log('executing query 2');
+			
 
 				if (results.length === 0){
 					//-----------The Access Code is Not Valid
@@ -47,48 +47,46 @@ qry_action.query('select * from admin_users where au_username = ? and au_passwor
 			}
 			else
 			{
-				console.log('executing query 3');
-				console.log(results[0]);
-				
+			
+		
 				var user_account = results[0].AU_ID; 
 
 
-				console.log('executing query 4');
+			
 				var hat = require('hat');
-				console.log('executing query 5');
+				
                 var access_code = hat().substring(0,4)+mdate.substring(0,3) ;
-                console.log('executing query 6');
-
+               
                 //------------------------CHECKING IF THE CLINIC ACCOUNT HAS ALREADY BEEN CREATED-------
                 qry_action.query('select * from mcm_clients_info where MCI_NAME = ? ',[newAccountDetails.clinic_name],function(err,results){
 					if (err)
 					{
-						console.log('executing query 7');
+						
 						
 
 					}
 					else
 					{
 						
-						console.log('executing query 8');
+						
 						if (results.length === 0){
-							console.log('executing query 9');
+						
 							//-------------Clinic is New------
 									//------------------------inserting into mcm_clients_info
 				qry_action.query('insert into mcm_clients_info set ?  ',{MCI_NAME:newAccountDetails.clinic_name,
 					MCM_LOCATION:newAccountDetails.clinic_address,MCI_MOB_TEL:newAccountDetails.clinic_tel,MCI_EMAIL:newAccountDetails.
 					clinic_email,MCI_ACCESS_CODE:access_code,MCI_STAFF_ID:user_account},function(err,results){
 
-console.log('executing query 10');
+
 					if (err){
-						console.log('executing query 11');
+						
 						throw err;
 					return   res.end(JSON.stringify({ resp:"err",err: 'Sorry, An error occured while creating this account' }));
-					console.log('executing query 12');
+					
 				}
 				else
 				{
-					console.log('executing query 13');
+				
 					return   res.end(JSON.stringify({ resp:"pass",access_code: access_code }));
 
 
@@ -98,7 +96,7 @@ console.log('executing query 10');
 						}
 						else
 						{
-							console.log('executing query 14');
+							
 							return   res.end(JSON.stringify({ resp:"err",err: 'Sorry, An exisiting account for this clinic is already created' }));
 
 
@@ -166,36 +164,42 @@ var m_time = time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds()
 qry_action.query('select * from mcm_clients_info where MCI_ACCESS_CODE = ? ',
 	[newAccountDetails.mci_access_code], 
 	function(err,results){
-
+		console.log('1');
 	if (err){
+		 console.log('2');
 			 res.end(JSON.stringify({ resp:"err",err: 'An error occured while executing the querry' }));
 		}
 		else
 		{
+			console.log('3');
 			if (results.length === 0){
+				console.log('4');
 					//-----------The Access Code is Not Valid
 					res.end(JSON.stringify({ resp:"err",err: 'Sorry the Clinic Access Code provided is not Valid' }));
 
 			}
 			else
 			{
+				console.log('5');
 				//-------------Clinic access code is valid----
 				qry_action.query('select * from users_info where ui_username = ? and ui_password = ? and ui_mci_access_code = ? ',
 	[newAccountDetails.username,newAccountDetails.password,newAccountDetails.mci_access_code],
 	function(err,results){
+		console.log('6');
 		//----if an error occurs on executing this sql statement
 		if (err){
+			console.log('7');
 			 res.end(JSON.stringify({ resp:"err",err: 'An error occured while executing the querry' }));
 		}
 		else
 		{
-           
+           console.log('8');
 		}
 
 		//------------if the account is new 
 		if (results.length === 0){
 			//-------------Create the new Account-----
-
+console.log('8');
 			qry_action.query('insert into users_info set ? ',{UI_SURNAME:newAccountDetails.surname,
 				UI_OTHERNAMES :newAccountDetails.othernames,UI_MOB_TEL:newAccountDetails.mob_tel,UI_USERNAME:newAccountDetails.username,
 				UI_PASSWORD :newAccountDetails.password,UI_MCI_ACCESS_CODE :newAccountDetails.mci_access_code},
@@ -203,11 +207,13 @@ qry_action.query('select * from mcm_clients_info where MCI_ACCESS_CODE = ? ',
 
 				//-----if an error occured
 				if (err){
+					console.log('10');
 					return   res.end(JSON.stringify({ resp:"err",err: 'Sorry, An error occured while creating this account' }));
 
 				}
 				else
 				{
+					console.log('11');
 					return   res.end(JSON.stringify({ resp:"pass",err: 'Account Successfully Created!!!' }));
 
 
@@ -222,6 +228,7 @@ qry_action.query('select * from mcm_clients_info where MCI_ACCESS_CODE = ? ',
 		}
 		else
 		{
+			console.log('12');
 			//------------ if the Account is already in the system
 			return   res.end(JSON.stringify({ resp:"err",err: 'Sorry, There is an existing account for this particular access code with the same account ID!!' }));
 
