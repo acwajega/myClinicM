adff32c71a09d3e8507195f2caec8671320bbe03
acwajega@wacsoftsolutions.com <acwajega@gmail.com>
2018-07-23 17:50:54 +0300

    sencondt

--- routes/route_index.js
+++ routes/route_index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+
+
+
+//-----------------------ACCOUNT DATA CONTROLLERS--------------------------
 var accounts_controller = require('../controllers/accounts_controller.js');
+//-----------------------END OF ACCOUNT DATA CONTROLLERS------------------
 
 
-var clinic_controller = require('../controllers/clinic_controller.js');
+//=======================================================================
+//------------CLINIC DATA CONTROLLERS--------------------------------
+
+//var clinic_controller = require('../controllers/clinic_controller.js');
+var clinic_controller_DailyIncomeTasks_dataUpload =require('../controllers/Clinic_data_controllers/Daily_IncomeTasks/data_upload.js');
+var clinic_controller_DailyIncomeTasks_dataSync =require('../controllers/Clinic_data_controllers/Daily_IncomeTasks/data_sync.js');
+
+
+//------------END OF CLINIC DATA CONTROLLERS--------------------------
+//=======================================================================
 
 
 
@@ -11,19 +25,20 @@ var clinic_controller = require('../controllers/clinic_controller.js');
 router.post('/api/accounts/createNewClinicAccount',accounts_controller.CreateNewClinicAccount);
 //---------------- End of New user account Creation ---------------------------------
 
+
 //-------------- routing to user api for New user account Creation ------------------
 router.post('/api/accounts/createNewUserAccount',accounts_controller.CreateNewUserAccount);
 //---------------- End of New user account Creation ---------------------------------
 
 
 //-------------- routing to Clinic api for Daily Income Upload ------------------
-router.post('/api/clinicData/Upload/DailyIncome',clinic_controller.UploadClinicDailyIncome);
+router.post('/api/clinicData/Upload/DailyIncome',clinic_controller_DailyIncomeTasks_dataUpload.Upload);
 //---------------- End of Daily Income Upload ---------------------------------
 
 
 
 //-------------- routing to Clinic api for Daily Income Sync ------------------
-router.post('/api/clinicData/Sync/DailyIncome',clinic_controller.SyncClinicDailyIncome);
+router.post('/api/clinicData/Sync/DailyIncome',clinic_controller_DailyIncomeTasks_dataSync.Sync);
 //---------------- End of Daily Income Upload ---------------------------------
 
 
